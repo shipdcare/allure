df271d11d3bac0f261624c6e2bc083c3242bb1e4
shipdcare <mg.mohak@gmail.com>
2017-07-19 10:15:25 +0530

    Deploy

--- allure/models.py
+++ allure/models.py
@@ -8,7 +8,7 @@ import json
 # DATABASE = PostgresqlDatabase(database=url.path[1:], user=url.username, password=url.password, host=url.hostname,
 #                         port=url.port)
 
-DATABASE = PostgresqlDatabase('allure', user='postgres', password="password", host="localhost", port="5432")
+DATABASE = PostgresqlDatabase('allure', user='postgres', password="password")
 
 
 class MyModel(Model):
